d346809bcd6b1cc6559d48aa5700b2cfd8a5d79e
Adrian Stier <adrianstier@adrians-mbp.lan>
2021-01-29 07:47:13 -0800

    updated pred survey code

--- Code/predator_surveys_graphs_statistics.R
+++ Code/predator_surveys_graphs_statistics.R
@@ -105,7 +105,45 @@ ggplot(df4,aes(x=type,y=fraction))+
 
 ggsave("figures/pred_survey/pred_survey_incidence_ native vs invasive predators.pdf")
 
-
+#alt figure
+  
+gg_dot <-  ggplot(data=df4) +
+  # remove axes and superfluous grids
+  theme_classic() +
+  theme(axis.title = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.line = element_blank()) + 
+  
+  # add a dummy point for scaling purposes
+  geom_point(aes(x = 0, y = site), 
+             size = 0, col = "white") + 
+  
+  # add the horizontal discipline lines
+  geom_hline(yintercept = 1:9, col = "grey80") +
+  
+  # add a point for each male success rate
+  geom_point(aes(x = fraction, y = site,color=type,pch=type), 
+             size = 11,alpha=0.5) +
+  # add the text (%) for each male success rate
+  geom_text(aes(x = fraction, y = site, 
+                label = paste0(round(fraction, 1))),
+            col = "black") +
+  # add the text (%) for each female success rate
+  # geom_text(aes(x = fraction, y = site, 
+  #               label = paste0(round(success_rates_women, 1))),
+  #           col = "white") +
+  # add a label above the first two points
+  # geom_text(aes(x = x, y = y, label = label, col = label),
+  #           data.frame(x = c(25.6 - 1.1, 26.5 + 0.6), y = 10, 
+  #                      label = c("women", "men")), size = 6) +
+  # scale_color_manual(values = c("#9DBEBB", "#468189"), guide = "none") +
+  
+  # manually specify the x-axis
+  scale_x_continuous(breaks = c(0,0.25,0.5,0.75,1), 
+                     labels = c("0%", "25%", "50%","75%","100%")) +
+  # manually set the spacing above and below the plot
+  scale_y_discrete(expand = c(0.2, 0)) 
+gg_dot
 #================================================================#
 #Density Analysis
 #================================================================#
